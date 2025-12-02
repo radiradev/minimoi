d7ef1153e7c2162fb7777ee9a55dbd022e8f6b02
radiradev <radi.radev.uk@gmail.com>
2025-12-02 17:06:41 +0100

    changes for meeting

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import fiddle as fdl
 from fiddle.experimental import auto_config
 from fiddle.codegen import codegen
 from fiddle import printing
-from fiddle import graphviz
 
 from datasets.dataset import Dataset
 from graphs.graph import Graph
@@ -16,7 +15,7 @@ def make_trainer():
     """
     dataset = Dataset(name="era", size=1000)
     # change the dataset to wrong type on purpose to trigger type checking error
-    dataset = 'ooops'
+    #dataset = 'ooops'
     graph = Graph(dataset=dataset, nodes=15)
 
     model = Model(graph=graph, hidden_dim=64)
@@ -28,8 +27,6 @@ def make_trainer():
 cfg = make_trainer.as_buildable()
 print(printing.as_str_flattened(cfg))
 
-out = graphviz.render(cfg)
-out.render("trainer_graph", format="png", cleanup=True)
 
 generated = codegen.codegen_dot_syntax(cfg)
 print("\n".join(generated.lines()))
